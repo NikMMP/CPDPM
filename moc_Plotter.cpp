a0008556645014161468ae28b53c519dfbc7584c
NikMMP <nikita.putin@pwc.ca>
2024-04-03 23:55:43 -0400

    random vibrations started

---
+++ moc_Plotter.cpp
@@ -0,0 +1,185 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'Plotter.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.12)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "Plotter.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'Plotter.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.12. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Plotter_t {
+    QByteArrayData data[15];
+    char stringdata0[216];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Plotter_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Plotter_t qt_meta_stringdata_Plotter = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "Plotter"
+QT_MOC_LITERAL(1, 8, 14), // "create_project"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 12), // "close_window"
+QT_MOC_LITERAL(4, 37, 12), // "save_project"
+QT_MOC_LITERAL(5, 50, 12), // "open_project"
+QT_MOC_LITERAL(6, 63, 21), // "create_line_from_file"
+QT_MOC_LITERAL(7, 85, 17), // "allow_cursor_grid"
+QT_MOC_LITERAL(8, 103, 14), // "create_x_title"
+QT_MOC_LITERAL(9, 118, 14), // "create_y_title"
+QT_MOC_LITERAL(10, 133, 14), // "create_x_range"
+QT_MOC_LITERAL(11, 148, 14), // "create_y_range"
+QT_MOC_LITERAL(12, 163, 19), // "allow_legend_action"
+QT_MOC_LITERAL(13, 183, 20), // "read_line_from_table"
+QT_MOC_LITERAL(14, 204, 11) // "rename_plot"
+
+    },
+    "Plotter\0create_project\0\0close_window\0"
+    "save_project\0open_project\0"
+    "create_line_from_file\0allow_cursor_grid\0"
+    "create_x_title\0create_y_title\0"
+    "create_x_range\0create_y_range\0"
+    "allow_legend_action\0read_line_from_table\0"
+    "rename_plot"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Plotter[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      13,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   79,    2, 0x08 /* Private */,
+       3,    0,   80,    2, 0x08 /* Private */,
+       4,    0,   81,    2, 0x08 /* Private */,
+       5,    0,   82,    2, 0x08 /* Private */,
+       6,    0,   83,    2, 0x08 /* Private */,
+       7,    0,   84,    2, 0x08 /* Private */,
+       8,    0,   85,    2, 0x08 /* Private */,
+       9,    0,   86,    2, 0x08 /* Private */,
+      10,    0,   87,    2, 0x08 /* Private */,
+      11,    0,   88,    2, 0x08 /* Private */,
+      12,    0,   89,    2, 0x08 /* Private */,
+      13,    0,   90,    2, 0x08 /* Private */,
+      14,    0,   91,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+
+       0        // eod
+};
+
+void Plotter::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Plotter *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: { int _r = _t->create_project();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 1: { int _r = _t->close_window();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 2: { int _r = _t->save_project();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 3: { int _r = _t->open_project();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 4: { int _r = _t->create_line_from_file();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 5: { int _r = _t->allow_cursor_grid();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 6: { int _r = _t->create_x_title();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 7: { int _r = _t->create_y_title();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 8: { int _r = _t->create_x_range();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 9: { int _r = _t->create_y_range();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 10: { int _r = _t->allow_legend_action();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 11: { int _r = _t->read_line_from_table();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 12: { int _r = _t->rename_plot();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Plotter::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_Plotter.data,
+    qt_meta_data_Plotter,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Plotter::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Plotter::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Plotter.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int Plotter::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 13)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 13;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 13)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 13;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ random-vibrations-truck/random-truck.py
@@ -0,0 +1,59 @@
+import numpy
+import matplotlib
+import sys 
+import math
+
+class Truck:
+   def __init__(self, data):
+
+     m = data["mass"]
+     jd = data["jd"]
+     c1 = data["c1"]
+     c2 = data["c2"]
+     b = data["b"]
+     l = data["l"]
+
+     self.bs1 = data["bs"]
+     self.as1 = data["as"]
+
+     a1 = data["a1"]
+     a2 = data["a2"]
+     v = data["v"]
+
+     self.M = numpy.array([ [m ,0.0], [0.0, jd] ])
+     self.K = numpy.array([ [ c1 + c2, c1 * b - c2 * (l-b)], [c1 * b + c2 * (l-b), c1 * pow(b,2) + c2 * pow(l-b,2)]]) 
+     self.C = numpy.array([ [ a1 + a2, a1 * b - a2 * (l - b)], [ a1 * b - a2 * (l - b), a1 * pow(b,2) + a2 * pow(l-b,2)]])
+     self.K1 = numpy.array([ [c1, c2], [c1 * b, -c2 * (l - b)] ])
+     self.C1 = numpy.array([ [a1, a2], [a1 * b, -a2 * (l - b)] ])
+     self.T = l / v
+     self.v = v
+
+   def input_psd(self, omega):
+     return self.as1 * self.v / ( 2 * math.pi * (pow(omega,2) + self.bs1 * pow(self.v,2)))
+
+
+if __name__ == "__main__":
+   print("Random Vibration Analysis")
+   data = {} ##contains input data for the analysis
+
+   if len(sys.argv) >= 2:
+    input_file = sys.argv[1]
+    with open(input_file,"r") as fh:
+      while True:
+        line = fh.readline()
+        if line:
+          #####print(line)
+          words = line.split(",")
+          data[words[0].lower()] = float(words[1])
+       
+        else:
+          break
+      truck = Truck(data)
+
+      for x in range(101):
+        omega = 2 * math.pi * x
+        y = truck.input_psd(omega)
+        print(omega,y)
+
+   else:
+    print("no input file")
