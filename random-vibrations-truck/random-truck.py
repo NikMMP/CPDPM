daf999d74350a16bb6f63b7ecf99157cc87c2c61
NikMMP <nikita.putin@pwc.ca>
2024-04-04 22:24:13 -0400

    calculated natural frequencies and normal modes

--- random-vibrations-truck/random-truck.py
+++ random-vibrations-truck/random-truck.py
@@ -21,7 +21,7 @@ class Truck:
      v = data["v"]
 
      self.M = numpy.array([ [m ,0.0], [0.0, jd] ])
-     self.K = numpy.array([ [ c1 + c2, c1 * b - c2 * (l-b)], [c1 * b + c2 * (l-b), c1 * pow(b,2) + c2 * pow(l-b,2)]]) 
+     self.K = numpy.array([ [ c1 + c2, c1 * b - c2 * (l-b)], [c1 * b - c2 * (l-b), c1 * pow(b,2) + c2 * pow(l-b,2)]]) 
      self.C = numpy.array([ [ a1 + a2, a1 * b - a2 * (l - b)], [ a1 * b - a2 * (l - b), a1 * pow(b,2) + a2 * pow(l-b,2)]])
      self.K1 = numpy.array([ [c1, c2], [c1 * b, -c2 * (l - b)] ])
      self.C1 = numpy.array([ [a1, a2], [a1 * b, -a2 * (l - b)] ])
@@ -33,7 +33,7 @@ class Truck:
 
 
 if __name__ == "__main__":
-   print("Random Vibration Analysis")
+   print("Random Vibration Analysis:")
    data = {} ##contains input data for the analysis
 
    if len(sys.argv) >= 2:
@@ -49,11 +49,30 @@ if __name__ == "__main__":
         else:
           break
       truck = Truck(data)
-
+      print()
+      print("input PSD:")
       for x in range(101):
         omega = 2 * math.pi * x
         y = truck.input_psd(omega)
         print(omega,y)
+      
+      print()
+      print("Natural frequencies, Hz:")
 
+      a = numpy.dot( numpy.linalg.inv(truck.M), truck.K)
+      eigenvalues, eigenvectors = numpy.linalg.eig(a)
+      for eig in eigenvalues:
+        print(math.sqrt(eig) / (2 * math.pi))
+
+      print()
+      print("Mode shapes in columns (normilized to mass matrix):")
+      norm = numpy.dot(eigenvectors.T,numpy.dot(truck.M, eigenvectors))
+      num_vectors = eigenvectors.shape[1] 
+      for i in range(num_vectors):
+        eigenvectors[:,i] = eigenvectors[:,i] / math.sqrt(norm[i,i])
+
+      print(eigenvectors)
+      ###norm = numpy.dot(eigenvectors.T,numpy.dot(truck.M, eigenvectors))
+      ####print(norm)
    else:
     print("no input file")
